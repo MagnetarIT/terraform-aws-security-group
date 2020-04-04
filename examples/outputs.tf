7393550ca6641b17277c14e1e1ff3888f605b4cd
Lewis Jenkins <lewis@magnetarconsulting.co.uk>
2020-04-04 19:01:06 +0100

    updates to output

--- examples/outputs.tf
+++ examples/outputs.tf
@@ -1,35 +1,35 @@
 output "security_group_id" {
   description = "The ID of the security group"
-  value       = aws_security_group.sg.id
+  value       = module.sg.security_group_id
 }
 
 output "security_group_arn" {
   description = "The ARN of the security group"
-  value       = aws_security_group.sg.arn
+  value       = module.sg.security_group_arn
 }
 
 output "vpc_id" {
   description = "The VPC ID."
-  value       = aws_security_group.sg.vpc_id
+  value       = module.sg.vpc_id
 }
 
 output "name" {
   description = "The name of the security group"
-  value       = aws_security_group.sg.name
+  value       = module.sg.name
 }
 
 output "description" {
   description = "The description of the security group"
-  value       = aws_security_group.sg.description
+  value       = module.sg.description
 }
 
 output "ingress" {
   description = "The ingress rules. See above for more."
-  value       = aws_security_group.sg.ingress
+  value       = module.sg.ingress
 }
 
 output "egress" {
   description = "The egress rules. See above for more."
-  value       = aws_security_group.sg.egress
+  value       = module.sg.egress
 }
 
